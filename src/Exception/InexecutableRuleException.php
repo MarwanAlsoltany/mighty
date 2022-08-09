f8c28d425a88cc0d33e353e82ce1e520a617642c
Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
2022-08-09 04:35:21 +0200

    Add inexecutable rule exception class * Add InexecutableRuleException.php

---
+++ src/Exception/InexecutableRuleException.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @author Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
+ * @copyright Marwan Al-Soltany 2022
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MAKS\Mighty\Exception;
+
+use RuntimeException as Exception;
+use MAKS\Mighty\Throwable;
+
+/**
+ * Exception thrown when validation rule execution fails.
+ *
+ * @package Mighty
+ */
+class InexecutableRuleException extends Exception implements Throwable
+{
+    // ...
+}
