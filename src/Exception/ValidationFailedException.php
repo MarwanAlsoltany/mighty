3f4b4e98cadab01835ffd4357ffc922910f86581
Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
2022-08-09 04:45:48 +0200

    Add validation failed exception class * Add ValidationFailedEception.php

---
+++ src/Exception/ValidationFailedException.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @author Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
+ * @copyright Marwan Al-Soltany 2022
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MAKS\Mighty\Exception;
+
+use UnexpectedValueException as Exception;
+use MAKS\Mighty\Exception\ValidatorThrowable;
+
+/**
+ * Exception thrown when validation is not successful or failed.
+ *
+ * @package Mighty\Validator
+ */
+class ValidationFailedException extends Exception implements ValidatorThrowable
+{
+    // ...
+}
