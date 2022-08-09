88e9f3885474cb5a22c1968d40a417f3afcb7bb6
Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
2022-08-09 04:38:34 +0200

    Add validator throwable * Add ValidatorThrowable.php

---
+++ src/Exception/ValidatorThrowable.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @author Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
+ * @copyright Marwan Al-Soltany 2022
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MAKS\Mighty\Exception;
+
+use MAKS\Mighty\Throwable;
+
+/**
+ * Validator throwable interface.
+ *
+ * @package Mighty\Validator
+ */
+interface ValidatorThrowable extends Throwable
+{
+    // ...
+}
