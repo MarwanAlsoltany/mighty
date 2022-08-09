0d415d696f5c89f09d25dc6a949371dfb03e9a4f
Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
2022-08-09 04:21:30 +0200

    Add throwable interface * Add Throwable.php

---
+++ src/Throwable.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @author Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
+ * @copyright Marwan Al-Soltany 2022
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MAKS\Mighty;
+
+use Throwable as CoreThrowable;
+
+/**
+ * Mighty throwable interface.
+ *
+ * @package Mighty
+ */
+interface Throwable extends CoreThrowable
+{
+    // ...
+}
