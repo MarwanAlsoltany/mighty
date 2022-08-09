01aa9eaaa8b15e9c0b6ce923ff98129e0391418c
Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
2022-08-09 04:58:08 +0200

    Add rule class * Add Rule.php

---
+++ src/Validation/Constraint/Rule.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * @author Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
+ * @copyright Marwan Al-Soltany 2022
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MAKS\Mighty\Validation\Constraint;
+
+use Attribute;
+use MAKS\Mighty\Engine;
+use MAKS\Mighty\Result;
+use MAKS\Mighty\Validation\Strategy;
+use MAKS\Mighty\Validation\Constraint;
+
+/**
+ * Validates any data using a single validation rule.
+ *
+ * @package Mighty\Validator
+ */
+#[Attribute(
+    Attribute::IS_REPEATABLE |
+    Attribute::TARGET_CLASS |
+    Attribute::TARGET_CLASS_CONSTANT |
+    Attribute::TARGET_PROPERTY |
+    Attribute::TARGET_METHOD
+)]
+class Rule extends Constraint implements ValidatesOne
+{
+    /**
+     * Rule constructor.
+     *
+     * @param string $name
+     * @param mixed[] $arguments
+     * @param string|null $message
+     * @param Strategy $strategy
+     */
+    public function __construct(
+        string $name,
+        ?array $arguments = null,
+        ?string $message = null,
+        Strategy $strategy = Strategy::FailLazy,
+    ) {
+        $validation = Engine::createRuleStatement($name, Engine::createRuleArguments($arguments ?? []));
+
+        parent::__construct(validation: $validation, messages: [$name => $message], strategy: $strategy);
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    public function validate(mixed $value = null): Result
+    {
+        return parent::validate($value);
+    }
+}
